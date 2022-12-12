f837bfbf4a430db5b8ff00aad955fff64cad6e2c
Kreato <kerimkocabas@tutanota.com>
2022-12-12 20:13:20 +0300

    feat: make remove work (mainly uses sh for removal for now)

--- src/nyaa/remove.nim
+++ src/nyaa/remove.nim
@@ -1,3 +1,18 @@
-proc remove(packages: seq[string]): string = 
+#import modules/chkroot
+
+proc remove(packages: seq[string], destdir=""): string =
   ## Remove packages
-  return ""
+  if packages.len == 0:
+    echo "error: please enter a package name"
+    quit(1)
+
+  for i in packages:
+    if not fileExists("/etc/nyaa.installed/"&i&"/list_files"):
+      echo "error: package "&i&" is not installed"
+      quit(1)
+    else:
+      for line in lines "/etc/nyaa.installed/"&i&"/list_files":
+       # TODO: replace this with a Nim implementation
+       discard execShellCmd("rm -f "&destdir&"/"&line&" 2>/dev/null")
+       discard execShellCmd("[ -z \"$(ls -A "&destdir&"/"&line&" 2>/dev/null)\" ] && rm -rf "&destdir&"/"&line&" 2>/dev/null")
+      echo "Package "&i&" removed."
