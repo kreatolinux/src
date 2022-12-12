db4dad36d6cbf8b63a59226a52dd132a7bde2d03
Kreato <kerimkocabas@tutanota.com>
2022-12-12 07:01:32 +0300

    feat: get info to work

--- src/nyaa/info.nim
+++ src/nyaa/info.nim
@@ -1,3 +1,13 @@
-proc info(packages: seq[string]): string = 
+proc info(repo="/etc/nyaa", package: seq[string]): string = 
   ## Get information about packages
-  return ""
+  discard parse_runfile(repo&"/"&package[0])
+  echo "package name: "&pkg
+  echo "package version: "&version
+  echo "package release: "&release
+  when declared(epoch):
+    echo "package epoch: "&epoch
+  if dirExists("/etc/nyaa.installed/"&package[0]):
+    return "installed: yes"
+  else:
+    return "installed: no"
+
