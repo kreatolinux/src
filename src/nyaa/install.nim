f7ff7135249c9013005152eeb0fa4a337943e375
Kreato <kerimkocabas@tutanota.com>
2022-12-13 17:19:12 +0300

    install: fix --yes not working, and some minor improvements

--- src/nyaa/install.nim
+++ src/nyaa/install.nim
@@ -1,18 +1,28 @@
-proc install_pkg(repo: string , package: string, root: string) =
+proc install_pkg(repo: string , package: string, root: string, binary=false) =
   ## Installs an package.
+  var tarball: string
+  
+  when declared(epoch):
+    tarball = "/etc/nyaa.tarballs/nyaa-tarball-"&pkg&"-"&version&"-"&release&"-"&epoch&".tar.gz"
+  else:
+    tarball = "/etc/nyaa.tarballs/nyaa-tarball-"&pkg&"-"&version&"-"&release&".tar.gz"
+
+  if sha256hexdigest(readAll(open(tarball)))&"  "&tarball != readAll(open(tarball&".sum")):
+    echo "error: sha256sum doesn't match"
+    quit(1)
+
   setCurrentDir("/etc/nyaa.tarballs")
-  parse_runfile(repo&"/"&package)
-    
+
+  if binary == true:
+    parse_runfile(repo&"/"&package&"-bin")
+  else:
+    parse_runfile(repo&"/"&package)
+   
   discard existsOrCreateDir("/etc/nyaa.installed")
   removeDir("/etc/nyaa.installed/"&package)
   copyDir(repo&"/"&package, "/etc/nyaa.installed/"&package)
 
-  # TODO: check for sha256sum before proceeding
-
-  when declared(epoch):
-    writeFile("/etc/nyaa.installed/"&package&"/list_files", execProcess("tar -xvf /etc/nyaa.tarballs/nyaa-tarball-"&pkg&"-"&version&"-"&release&"-"&epoch&".tar.gz -C "&root))
-  else:
-    writeFile("/etc/nyaa.installed/"&package&"/list_files", execProcess("tar -xvf /etc/nyaa.tarballs/nyaa-tarball-"&pkg&"-"&version&"-"&release&".tar.gz -C "&root))
+  writeFile("/etc/nyaa.installed/"&package&"/list_files", execProcess("tar -xvf"&tarball&" -C "&root))
   
 proc install(packages: seq[string], root="/", yes=false, no=false, binrepo="mirror.kreato.dev", repo="/etc/nyaa-bin"): string =
   ## Fast and efficient package manager
@@ -34,22 +44,24 @@ proc install(packages: seq[string], root="/", yes=false, no=false, binrepo="mirr
 
   echo "Packages:"&res
 
+  var output: string
+
   if yes != true and no != true:
     stdout.write "Do you want to continue? (y/N) "
-    var output = readLine(stdin)
+    output = readLine(stdin)
 
-    if output == "y" or output == "Y" or yes == true:
-      for i in packages:
-        parse_runfile(repo&"/"&i&"-bin")
-        discard existsOrCreateDir("/etc/nyaa.tarballs")
-        let tarball = "nyaa-tarball-"&i&"-"&version&"-"&release&".tar.gz"
-        let chksum = tarball&".sum"
-        setCurrentDir("/etc/nyaa.tarballs")
-        echo "https://"&binrepo&"/"&tarball
-        writeFile(tarball, fetch("https://"&binrepo&"/"&tarball))
-        writeFile(chksum, fetch("https://"&binrepo&"/"&chksum))
-
-        install_pkg(repo, i&"-bin", root)
-        echo "Installation for "&i&" complete"
-    else:
+  if output == "y" or output == "Y" or yes == true:
+    for i in packages:
+      parse_runfile(repo&"/"&i&"-bin")
+      discard existsOrCreateDir("/etc/nyaa.tarballs")
+      let tarball = "nyaa-tarball-"&i&"-"&version&"-"&release&".tar.gz"
+      let chksum = tarball&".sum"
+      setCurrentDir("/etc/nyaa.tarballs")
+      echo "Downloading tarball"
+      writeFile(tarball, fetch("https://"&binrepo&"/"&tarball))
+      echo "Downloading tarball checksum"
+      writeFile(chksum, fetch("https://"&binrepo&"/"&chksum))
+      install_pkg(repo, i, root, true)
+      return "Installation for "&i&" complete"
+  else:
       return "nyaa: exiting"
