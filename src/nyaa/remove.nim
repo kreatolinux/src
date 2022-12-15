5af0a88d8c0eaf710db23c13f37b3634172d5815
Kreato <kerimkocabas@tutanota.com>
2022-12-15 16:27:22 +0300

    remove: change --destdir to --root for compatibility

--- src/nyaa/remove.nim
+++ src/nyaa/remove.nim
@@ -1,4 +1,4 @@
-proc remove(packages: seq[string], yes = false, destdir = ""): string =
+proc remove(packages: seq[string], yes = false, root = ""): string =
     ## Remove packages
     if packages.len == 0:
         err("please enter a package name", false)
@@ -21,8 +21,8 @@ proc remove(packages: seq[string], yes = false, destdir = ""): string =
 
             for line in lines "/etc/nyaa.installed/"&i&"/list_files":
                 # TODO: replace this with a Nim implementation
-                discard execShellCmd("rm -f "&destdir&"/"&line&" 2>/dev/null")
-                discard execShellCmd("[ -z \"$(ls -A "&destdir&"/"&line&" 2>/dev/null)\" ] && rm -rf "&destdir&"/"&line&" 2>/dev/null")
+                discard execShellCmd("rm -f "&root&"/"&line&" 2>/dev/null")
+                discard execShellCmd("[ -z \"$(ls -A "&root&"/"&line&" 2>/dev/null)\" ] && rm -rf "&root&"/"&line&" 2>/dev/null")
             return "Package "&i&" removed."
 
     return "Exiting."
