6ea3fc189a20fef8485ec812d2968069958d8f84
Kreato <kerimkocabas@tutanota.com>
2022-12-17 00:44:48 +0300

    removeInternal: fix root variable

--- src/nyaa/modules/removeInternal.nim
+++ src/nyaa/modules/removeInternal.nim
@@ -1,5 +1,5 @@
 proc removeInternal(package: string, root = ""): string =
-    if not fileExists("/etc/nyaa.installed/"&package&"/list_files"):
+    if not fileExists(root&"/etc/nyaa.installed/"&package&"/list_files"):
         err("package "&package&" is not installed", false)
 
     for line in lines root&"/etc/nyaa.installed/"&package&"/list_files":
