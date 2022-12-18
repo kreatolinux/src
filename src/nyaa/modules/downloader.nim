43cf67c49240ec164e513d0fdd9a5ed4bb7c169a
Kreato <kerimkocabas@tutanota.com>
2022-12-18 04:52:17 +0000

    Commit from GitHub Actions (Format Nim Files)

--- src/nyaa/modules/downloader.nim
+++ src/nyaa/modules/downloader.nim
@@ -3,7 +3,8 @@ import terminal, math
 
 proc onProgressChanged(total, progress, speed: BiggestInt) {.async.} =
   stdout.eraseLine
-  stdout.write("Downloaded ", formatSize(progress), " of ", formatSize(total), " at ", formatSize(speed), "/s ", round(int(progress) / int(total)*100),"% complete")
+  stdout.write("Downloaded ", formatSize(progress), " of ", formatSize(total),
+      " at ", formatSize(speed), "/s ", round(int(progress) / int(total)*100), "% complete")
   stdout.flushFile
 
 proc download(url: string, file: string) {.async.} =
