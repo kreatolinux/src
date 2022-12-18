20a4e0fcace34a808920704dc833127e368e7351
Kreato <kerimkocabas@tutanota.com>
2022-12-18 14:16:43 +0000

    Commit from GitHub Actions (Format Nim Files)

--- src/nyaa/modules/dephandler.nim
+++ src/nyaa/modules/dephandler.nim
@@ -8,13 +8,13 @@ proc dephandler(pkgs: seq[string]): seq[string] =
             let repo = findPkgRepo(pkg)
             if repo == "":
                 err("Package "&pkg&" doesn't exist", false)
-            
+
             if fileExists(repo&"/"&pkg&"/deps"):
-              for dep in openFileStream(repo&"/"&pkg&"/deps", fmRead).lines():
-                  if dep in deps or dep in pkgs:
-                      continue
-                  deps.add(dephandler(@[dep]))
-                  deps.add(dep)
-              return deps
+                for dep in openFileStream(repo&"/"&pkg&"/deps", fmRead).lines():
+                    if dep in deps or dep in pkgs:
+                        continue
+                    deps.add(dephandler(@[dep]))
+                    deps.add(dep)
+                return deps
     except Exception:
         raise
