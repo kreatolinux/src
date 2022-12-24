e7e570669d0b7ba07c052628cb3a38c171e6e3e8
Kreato <kerimkocabas@tutanota.com>
2022-12-24 17:26:57 +0300

    update: fix multiple bugs

--- src/nyaa/update.nim
+++ src/nyaa/update.nim
@@ -1,16 +1,28 @@
-proc update(repo = "https://github.com/kreatolinux/nyaa-repo.git",
-    path = "/etc/nyaa"): string =
+proc update(repo = "",
+    path = ""): string =
     ## Update repositories
-    if dirExists(path):
-        if execShellCmd("git -C "&path&" pull") != 0:
-            err("failed to update repositories!", false)
-    else:
-        echo "nyaa: repositories not found, cloning them now..."
-        discard execProcess("git clone "&repo&" "&path)
-        let repolinks = getConfigValue("Repositories", "RepoLinks")
-        let repodirs = getConfigValue("Repositories", "RepoDirs")
-        if not (repo in repolinks and path in repodirs):
-            setConfigValue("Repositories", "RepoLinks", repolinks&" "&repo)
-            setConfigValue("Repositories", "RepoDirs", repodirs&" "&path)
+
+    if isAdmin() == false:
+      err("you have to be root for this action.", false)
+  
+    let repodirs = getConfigValue("Repositories", "RepoDirs")
+    let repolinks = getConfigValue("Repositories", "RepoLinks")
+    
+    for i in repodirs.split(" "):
+      if dirExists(i):
+          if execShellCmd("git -C "&i&" pull") != 0:
+              err("failed to update repositories!", false)
+      else:
+        echo "nyaa: repository on "&i&" not found, cloning them now..."
+        for l in repolinks.split(" "):
+          discard execProcess("git clone "&l&" "&i)
+    
+    if path != "" and repo != "":
+      echo "cloning "&path&" from "&repo
+      discard execProcess("git clone "&repo&" "&path)
+      if not (repo in repolinks and path in repodirs):
+        setConfigValue("Repositories", "RepoLinks", repolinks&" "&repo)
+        setConfigValue("Repositories", "RepoDirs", repodirs&" "&path)
+
 
     return "nyaa: updated all repositories"
