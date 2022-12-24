d85b62ffaae537a5d9a48c62796bca44230fcc01
Kreato <kerimkocabas@tutanota.com>
2022-12-24 17:54:02 +0300

    update: cleanup

--- src/nyaa/update.nim
+++ src/nyaa/update.nim
@@ -4,18 +4,19 @@ proc update(repo = "",
 
     if isAdmin() == false:
         err("you have to be root for this action.", false)
-
+    
     let repodirs = getConfigValue("Repositories", "RepoDirs")
     let repolinks = getConfigValue("Repositories", "RepoLinks")
 
-    for i in repodirs.split(" "):
-        if dirExists(i):
-            if execShellCmd("git -C "&i&" pull") != 0:
+    let repoList: seq[tuple[dir: string, link: string]] = zip(repodirs.split(" "), repolinks.split(" "))
+    
+    for i in repoList:
+        if dirExists(i.dir):
+            if execShellCmd("git -C "&i.dir&" pull") != 0:
                 err("failed to update repositories!", false)
         else:
-            echo "nyaa: repository on "&i&" not found, cloning them now..."
-            for l in repolinks.split(" "):
-                discard execProcess("git clone "&l&" "&i)
+            echo "nyaa: repository on "&i.dir&" not found, cloning them now..."
+            discard execProcess("git clone "&i.link&" "&i.dir)
 
     if path != "" and repo != "":
         echo "cloning "&path&" from "&repo
