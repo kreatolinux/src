dae3ef73caf2b467c145f0a507d3f9e3762d432d
Kreato <kreato@kreato.dev>
2022-12-30 23:13:01 +0300

    purr: check dephandler

--- purr/purr.nim
+++ purr/purr.nim
@@ -60,6 +60,11 @@ proc purr(tests = "all", tmpdir = "/tmp/purr") =
         ok("remove test completed succesfully")
     else:
         error("remove test failed")
+    
+    if dephandler(toSeq(["sway"])).join(" ") != "pcre expat openssl zlib libxcrypt python libxml2 ninja meson wayland samurai wayland-protocols libpciaccess libdrm xkeyboard-config libxkbcommon libevdev mtdev gmake libudev libinput seatd pixman libelf python-mako m4 gmp isl libzstd flex perl texinfo file binutils mpc mpfr linux-headers gcc autoconf automake libtool libuv libarchive nghttp2 curl cmake llvm mesa wlroots libpng freetype-harfbuzz pkgconf cairo git glib fribidi gperf fontconfig pango json-c":
+        error("dephandler test failed")
+    else:
+        ok("dephandler test completed successfully")
 
     discard info(toSeq(["purr"]))
     ok("info test completed")
