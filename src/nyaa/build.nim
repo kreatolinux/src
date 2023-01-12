1dd2830747bf2eabd2357023a2201cdf2817d696
PandaNinjas <admin@malwarefight.gq>
2023-01-12 00:46:43 +0000

    Make it tell you the expected and the actual

--- src/nyaa/build.nim
+++ src/nyaa/build.nim
@@ -75,10 +75,11 @@ proc builder(package: string, destdir: string,
             waitFor download(i.replace("$VERSION", pkg.version), filename)
         except:
             raise
-
-        if sha256hexdigest(readAll(open(filename)))&"  "&filename !=
-                pkg.sha256sum.split(";")[int]:
-            err "sha256sum doesn't match for "&i
+        
+        var expectedDigest = sha256hexdigest(readAll(open(filename)))&"  "&filename
+        var actualDigest = pkg.sha256sum.split(";")[int]
+        if expectedDigest != actualDigest:
+            err "sha256sum doesn't match for "&i&"\nExpected: "&expectedDigest&"\nActual: "&actualDigest
 
         int = int+1
 
