5ce4d21cafd3befa477a0c3b26038b8efc02a882
Kreato <kreato@kreato.dev>
2023-01-28 00:54:33 +0300

    Install python and pip onto the rootfs

--- src/nyaastrap/nyaastrap.nim
+++ src/nyaastrap/nyaastrap.nim
@@ -216,12 +216,24 @@ proc rootfs(buildType = "builder", arch = "amd64",
             ok "Generated CA certificates"
 
         removeFile(buildDir&"/certdata.txt")
+        
+        nyaastrapInstall("python", installWithBinaries, buildDir, useCacheIfPossible)
+        
+        let ensurePip = execCmdEx("chroot "&buildDir&" /bin/sh -c 'python -m ensurepip'")
+        
+        if ensurePip.exitcode != 0:
+            debug "ensurePip output: "&ensurePip.output
+            error "Installing pip failed"
+        else:
+            ok "Installed pip"
 
         if conf.getSectionValue("Extras", "ExtraPackages") != "":
             info_msg "Installing extra packages"
             for i in conf.getSectionValue("Extras", "ExtraPackages").split(" "):
                 nyaastrapInstall(i, installWithBinaries, buildDir, useCacheIfPossible)
 
+
+        
 proc buildPackages(useCacheIfPossible = true, repo = "/etc/nyaa") =
     ## Build all packages available.
 
