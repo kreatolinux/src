f4294f4ef308cf71516cf0770704d44b4ef33ee3
Kreato <kreato@kreato.dev>
2023-02-14 03:52:15 +0300

    build.sh improvements

--- build.sh
+++ build.sh
@@ -2,28 +2,39 @@
 OUTDIR="./out/"
 SRCDIR="./src/"
 
+run() {
+        echo "Running $@"
+        "$@"
+}
+
 case $1 in
         "kpkg")
-                nim c -d:release -d:branch=master --threads:on -d:ssl -o="$OUTDIR" "$SRCDIR/kpkg/kpkg.nim"
+                shift
+                run nim c -d:release -d:branch=master --threads:on -d:ssl "$@" -o="$OUTDIR" "$SRCDIR/kpkg/kpkg.nim"
         ;;
         "prettify")
-                nimpretty src/*/*/*              
+                run nimpretty src/*/*/*              
         ;;
         "tests")
-                nim c -d:release --threads:on -d:ssl -o="$OUTDIR" "$SRCDIR/purr/purr.nim"
+                shift
+                run nim c -d:release --threads:on -d:ssl "$@" -o="$OUTDIR" "$SRCDIR/purr/purr.nim"
         ;;
         "chkupd")                    
-                nim c -d:release -d:ssl -o="$OUTDIR" "$SRCDIR/chkupd/chkupd.nim"              
+                shift
+                run nim c -d:release -d:ssl "$@" -o="$OUTDIR" "$SRCDIR/chkupd/chkupd.nim"              
         ;;
         "kreastrap")
-                nim c --threads:on -d:ssl -o="$SRCDIR/kreastrap/kreastrap" "$SRCDIR/kreastrap/kreastrap.nim"
+                shift
+                run nim c --threads:on -d:ssl "$@" -o="$SRCDIR/kreastrap/kreastrap" "$SRCDIR/kreastrap/kreastrap.nim"
         ;;
         "deps")
-                nimble install cligen libsha -y
+                shift
+                run nimble install cligen libsha "$@" -y
                 ;;
         *)
                 echo """./build.sh kpkg: builds kpkg
 ./build.sh prettify: uses nimpretty to prettify code
+./build.sh tests: builds tests
 ./build.sh chkupd: builds chkupd
 ./build.sh kreastrap: builds kreastrap
 ./build.sh deps: Install dependencies through Nimble"""
