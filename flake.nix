e35f198a73723f98e2dc880203b87050d9280d15
seth <getchoo@tuta.io>
2023-02-24 06:22:04 -0500

    modularize flake + add nimpretty

--- flake.nix
+++ flake.nix
@@ -17,103 +17,98 @@
     lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
     version = builtins.substring 0 8 lastModifiedDate;
     supportedSystems = ["x86_64-linux"];
+
     forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
     nixpkgsFor = forAllSystems (system:
       import nixpkgs {
         inherit system;
         overlays = [nimble.overlay];
       });
-    nimBuild = "nim c -d:release -d:branch-master --threads:on -d:ssl --nimcache:.cache/";
   in {
     packages =
       forAllSystems
       (system: let
         pkgs = nixpkgsFor.${system};
-      in rec {
-        kpkg = pkgs.stdenv.mkDerivation rec {
-          pname = "kpkg";
-          inherit version;
 
-          src = ./.;
-          nativeBuildInputs = with pkgs; with pkgs.nimPackages; [nim cligen libsha];
+        libArgs = {name}: "-p:${pkgs.nimPackages.${name}}";
 
-          buildPhase = ''
-            ${nimBuild} -p:${pkgs.nimPackages.cligen} -p:${pkgs.nimPackages.libsha} -o=./out/${pname} ./src/${pname}/${pname}.nim
-          '';
+        nimBuild = {
+          name,
+          nativeBuild ? [],
+          buildInputs ? [],
+          args ? "",
+        }: {
+          ${name} = with pkgs;
+            stdenv.mkDerivation rec {
+              pname = name;
+              inherit buildInputs version;
 
-          installPhase = ''
-            mkdir -p $out/bin
-            cp out/${pname} $out/bin/
-          '';
+              src = ./.;
+              nativeBuildInputs = [nim] ++ nativeBuild;
+
+              buildPhase = ''
+                nim compile -d:release -d:branch-master --threads:on -d:ssl --nimcache:.cache/ \
+                  ${args} \
+                  -o=./out/${pname} ./src/${pname}/${pname}.nim
+              '';
+
+              installPhase = ''
+                mkdir -p $out/bin
+                cp out/${pname} $out/bin/
+              '';
+            };
         };
-        chkupd = pkgs.stdenv.mkDerivation rec {
-          pname = "chkupd";
-          inherit version;
 
-          src = ./.;
-          nativeBuildInputs = with pkgs; with pkgs.nimPackages; [nim cligen libsha];
-
-          buildPhase = ''
-            ${nimBuild} -p:${pkgs.nimPackages.cligen} -p:${pkgs.nimPackages.libsha} -o=./out/${pname} ./src/${pname}/${pname}.nim
-          '';
-
-          installPhase = ''
-            mkdir -p $out/bin
-            cp out/${pname} $out/bin/
-          '';
+        cligenArgs = libArgs {
+          name = "cligen";
         };
-        mari = pkgs.stdenv.mkDerivation rec {
-          pname = "mari";
-          inherit version;
-
-          src = ./.;
-          nativeBuildInputs = with pkgs; with pkgs.nimPackages; [nim httpbeast libsha];
-
-          buildPhase = ''
-            ${nimBuild} -o=./out/${pname} ./src/${pname}/${pname}.nim
-          '';
-
-          installPhase = ''
-            mkdir -p $out/bin
-            cp out/${pname} $out/bin/
-          '';
+        httpbeastArgs = libArgs {
+          name = "httpbeast";
         };
-        purr = pkgs.stdenv.mkDerivation rec {
-          pname = "purr";
-          inherit version;
-
-          src = ./.;
-
-          buildInputs = [kpkg];
-          nativeBuildInputs = with pkgs; with pkgs.nimPackages; [nim cligen] ++ buildInputs;
-
-          buildPhase = ''
-            ${nimBuild} -p:${pkgs.nimPackages.libsha} -p:${pkgs.nimPackages.cligen} -o=./out/${pname} ./src/${pname}/${pname}.nim
-          '';
-
-          installPhase = ''
-            mkdir -p $out/bin
-            cp out/${pname} $out/bin
-          '';
+        libshaArgs = libArgs {
+          name = "libsha";
         };
-        kreastrap = pkgs.stdenv.mkDerivation rec {
-          pname = "kreastrap";
-          inherit version;
+      in
+        with pkgs;
+          (nimBuild {
+            name = "kpkg";
+            nativeBuild = with nimPackages; [cligen libsha];
+            args = "${cligenArgs} ${libshaArgs}";
+          })
+          // (nimBuild {
+            name = "chkupd";
+            nativeBuild = with nimPackages; [cligen libsha];
+            args = "${cligenArgs} ${libshaArgs}";
+          })
+          // (nimBuild {
+            name = "mari";
+            nativeBuild = with nimPackages; [httpbeast libsha];
+            args = "${httpbeastArgs} ${libshaArgs}";
+          })
+          // (nimBuild {
+            name = "purr";
+            nativeBuild = with nimPackages; [cligen];
+            args = cligenArgs;
+            buildInputs = [self.packages.${system}.kpkg];
+          })
+          // (nimBuild {
+            name = "kreastrap";
+            buildInputs = [self.packages.${system}.purr];
+            nativeBuild = buildInputs;
+          }));
 
-          src = ./.;
-
-          buildInputs = [purr];
-          nativeBuildInputs = with pkgs; [nim] ++ buildInputs;
-
-          buildPhase = ''
-            ${nimBuild} -p:${pkgs.nimPackages.cligen} -p:${pkgs.nimPackages.libsha} -o=./out/${pname} ./src/${pname}/${pname}.nim
+    checks = forAllSystems (system: let
+      pkgs = nixpkgsFor.${system};
+    in
+      with pkgs; {
+        nimpretty =
+          runCommand "nimpretty" {
+            buildInputs = [nim];
+          }
+          ''
+            mkdir $out
+            find ${./src} -type f -name '*.nim' | xargs nimpretty
           '';
-
-          installPhase = ''
-            mkdir -p $out/bin
-            cp out/${pname} $out/bin
-          '';
-        };
       });
 
     defaultPackage = forAllSystems (system: self.packages.${system}.kpkg);
@@ -122,20 +117,8 @@
       pkgs = nixpkgsFor.${system};
     in
       with pkgs;
-      with pkgs.nimPackages;
         mkShell {
-          packages = [gnumake nim cligen libsha];
+          packages = with nimPackages; [gnumake nim cligen libsha];
         });
-
-    meta = with nixpkgs.lib; {
-      homepage = "https://github.com/kreatolinux/src";
-      description = "Kreato Linux source tree";
-      longDescription = ''
-        The toolset with everything you need to build, test, and maintain Kreato Linux
-      '';
-      platforms = platforms.linux;
-      license = licenses.gpl3Only;
-      maintainers = with maintainers; [kreato getchoo];
-    };
   };
 }
