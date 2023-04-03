dbc2218bcef825949f8c3b681c51b3a3d84462a5
Kreato <kreato@kreato.dev>
2023-04-04 02:08:46 +0300

    kpkg: build: useCacheIfAvailable: install on root too

--- src/kpkg/build.nim
+++ src/kpkg/build.nim
@@ -66,6 +66,7 @@ proc builder(package: string, destdir: string,
             fileExists(
             "/var/cache/kpkg/archives/arch/"&hostCPU&"/kpkg-tarball-"&pkg.pkg&"-"&pkg.versionString&".tar.gz.sum") and
             useCacheIfAvailable == true and dontInstall == false:
+        install_pkg(repo, package, "/") # Install package on root too
         install_pkg(repo, package, destdir)
         removeFile(lockfile)
         return true
