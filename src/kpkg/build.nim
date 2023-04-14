6e87f45cab5cbfaa58b221fd6c19f13834cedf7b
Kreato <kreato@kreato.dev>
2023-04-14 15:47:40 +0300

    fix errors about last commit

--- src/kpkg/build.nim
+++ src/kpkg/build.nim
@@ -117,13 +117,13 @@ proc builder(package: string, destdir: string,
                 0, "prepare failed"
 
     var cmd: int
-    var cmd2: tuple[output: string, exitCode: int]
+    var cmd2: int
 
     if pkg.sources.split(";").len == 1:
         if existsPrepare == 0:
             cmd = execShellCmd("su -s /bin/sh _kpkg -c '. "&path&"/run"&" && export CC="&getConfigValue(
                     "Options", "cc")&" && build'")
-            cmd2 = execCmdEx(". "&path&"/run"&" && export DESTDIR="&root&" && export ROOT=$DESTDIR && install")
+            cmd2 = execShellCmd(". "&path&"/run"&" && export DESTDIR="&root&" && export ROOT=$DESTDIR && install")
         else:
             cmd = execShellCmd("su -s /bin/sh _kpkg -c 'cd "&folder[
                     0]&" && . "&path&"/run"&" && export CC="&getConfigValue(
