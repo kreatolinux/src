701f1cb904d7095169f6d6463881f0ea4d3aba86
Kreato <kreato@kreato.dev>
2023-04-27 13:45:12 +0000

    Commit from GitHub Actions (Format Nim Files)

--- src/kpkg/install.nim
+++ src/kpkg/install.nim
@@ -62,7 +62,7 @@ proc install_pkg(repo: string, package: string, root: string, binary = false,
             else:
                 echo "kpkg: reproducibility check failed"
                 echo "kpkg: run with --enforceReproducibility=true if you want to enforce this"
-        
+
         discard execProcess("tar -hxf"&tarball&" -C "&root)
     else:
         discard execProcess("tar -hxf"&tarball&" -C "&root)
