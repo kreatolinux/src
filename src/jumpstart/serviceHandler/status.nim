cb72c31d264544228d47c8f8de33878ba92cc164
Kreato <kreato@kreato.dev>
2023-05-07 19:12:07 +0000

    Commit from GitHub Actions (Format Nim Files)

--- src/jumpstart/serviceHandler/status.nim
+++ src/jumpstart/serviceHandler/status.nim
@@ -3,16 +3,17 @@ import os
 import strutils
 
 proc statusDaemon*(process: Process, serviceName: string) =
-    
+
     if not fileExists("/run/serviceHandler/"&serviceName&"/status"):
         writeFile("/run/serviceHandler/"&serviceName&"/status", "running")
-    
+
     let exited = waitForExit(process)
 
     if exited == 0:
         writeFile("/run/serviceHandler/"&serviceName&"/status", "stopped")
     else:
-        writeFile("/run/serviceHandler/"&serviceName&"/status", "stopped with an exit code "&intToStr(exited))
+        writeFile("/run/serviceHandler/"&serviceName&"/status",
+                "stopped with an exit code "&intToStr(exited))
 
     while true:
         if fileExists("/run/serviceHandler/"&serviceName&"/stopFile"):
