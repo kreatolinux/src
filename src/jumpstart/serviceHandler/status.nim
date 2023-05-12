050ecde4a328312492ec585fe7802fde821b37ca
Kreato <kreato@kreato.dev>
2023-05-12 15:23:58 +0000

    Commit from GitHub Actions (Format Nim Files)

--- src/jumpstart/serviceHandler/status.nim
+++ src/jumpstart/serviceHandler/status.nim
@@ -1,7 +1,8 @@
 import osproc
 import os
 import strutils
-proc statusDaemon*(process: Process, serviceName: string, command: string,  options: set[ProcessOption]) =
+proc statusDaemon*(process: Process, serviceName: string, command: string,
+        options: set[ProcessOption]) =
     ## Reports the status, also runs execPost of the service.
     if not fileExists("/run/serviceHandler/"&serviceName&"/status"):
         writeFile("/run/serviceHandler/"&serviceName&"/status", "running")
@@ -9,7 +10,7 @@ proc statusDaemon*(process: Process, serviceName: string, command: string,  opti
     try:
         let exited = waitForExit(process)
         if exited == 0:
-            let processPost = startProcess(command=command, options=options)
+            let processPost = startProcess(command = command, options = options)
             discard waitForExit(processPost) # We don't care about the exit code of execPost for now
             writeFile("/run/serviceHandler/"&serviceName&"/status", "stopped")
         else:
