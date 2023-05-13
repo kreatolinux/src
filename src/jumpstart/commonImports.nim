47a196f40c01ffab83b945d3e61cdbd8bf3f178c
Kreato <kreato@kreato.dev>
2023-05-13 07:17:29 +0300

    Jumpstart v1.0.0

--- src/jumpstart/commonImports.nim
+++ src/jumpstart/commonImports.nim
@@ -1,6 +1,6 @@
 # Jumpstart configuration
 # Here, you can find the usual stuff.
-const jumpstartVersion = "jumpstart v1-alpha"
+const jumpstartVersion = "jumpstart v1.0.0"
 const sockPath = "/run/jumpstart.sock"
 const servicePath = "/etc/jumpstart/services"
 const mountPath = "/etc/jumpstart/mounts"
