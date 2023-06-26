971cfcc80dd3ac85425d4d61f9e98e84e60ac205
Kreato <kreato@kreato.dev>
2023-06-26 21:33:17 +0300

    [skip ci] chkupd: fix the simple script

--- src/chkupd/jsontomd.sh
+++ src/chkupd/jsontomd.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 # Simple script to convert chkupd json to markdown.
-SUCCESSFUL_PKG_COUNT="$(cat $1 | jq -r .successfulPkgCount)"
-FAILED_PKG_COUNT="$(cat $1 | jq -r .failedPkgCount)"
-FAILED_BUILD_PACKAGES="$(cat $1 | jq -r .failedBuildPackages)"
-FAILED_UPD_PACKAGES="$(cat $1 | jq -r .failedUpdPackages)"
-
 echo "# chkupd automatic update report"
 echo
-echo "$SUCCESSFUL_PKG_COUNT packages autoupdated successfully. $FAILED_PKG_COUNT packages failed to autoupdate."
+echo "$(cat $1 | jq -r .[].successfulPkgCount) packages updated successfully. $(cat $1 | jq -r .[].failedPkgCount) packages failed to autoupdate."
 echo
 echo "# Packages that failed to build"
-echo "$FAILED_BUILD_PACKAGES"
+for i in $(cat $1 | jq -r .[].failedBuildPackages[]); do
+        echo "* $(basename $i)"
+done
 echo
 echo "# Packages that failed to autoupdate"
-echo "$FAILED_UPD_PACKAGES"
+for i in $(cat $1 | jq -r .[].failedUpdPackages[]); do
+        echo "* $(basename $i)"
+done
