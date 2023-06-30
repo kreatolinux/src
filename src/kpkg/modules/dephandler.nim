8b77fbad40816c8a31fa3c1bd581d795f1a7eda2
Kreato <kreato@kreato.dev>
2023-06-30 11:31:39 +0000

    Commit from GitHub Actions (Format Nim Files)

--- src/kpkg/modules/dephandler.nim
+++ src/kpkg/modules/dephandler.nim
@@ -25,10 +25,10 @@ proc dephandler(pkgs: seq[string], ignoreDeps = @["  "], bdeps = false): seq[str
 
             if fileExists(repo&"/"&pkg&depsType):
                 for dep in lines repo&"/"&pkg&depsType:
-                    
+
                     if fileExists(repo&"/"&dep&"/build_deps"):
                         deps.add(dephandler(@[dep], deps&ignoreDeps, bdeps = true))
-                    
+
                     if dep in pkgs or dep in deps or isIn(deps, ignoreDeps) or
                             dep in ignoreDeps:
                         continue
