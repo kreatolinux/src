7236fb7bddd73acd5a95da3ab9bb0b494384bb6c
Kreato <kreato@kreato.dev>
2023-07-28 08:55:32 +0300

    kpkg: install: feat: add basic postinstall support

--- src/kpkg/install.nim
+++ src/kpkg/install.nim
@@ -67,6 +67,11 @@ proc install_pkg(repo: string, package: string, root: string, binary = false,
 
     # Run ldconfig afterwards for any new libraries
     discard execProcess("ldconfig")
+    
+    var existsPostinstall = execShellCmd(". "&repo&"/"&package&"/run"&" && command -v postinstall")
+
+    if existsPostinstall == 0:
+        execShellCmd(". "&repo&"/"&package&"/run"&" && postinstall")
 
 proc install_bin(packages: seq[string], binrepo: string, root: string,
         offline: bool, downloadOnly = false) =
