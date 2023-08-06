2d07d26118a2aa7c455bb34342b3092e5b91c656
Kreato <kreato@kreato.dev>
2023-08-06 06:31:47 +0000

    Commit from GitHub Actions (Format Nim Files)

--- kpkg/modules/downloader.nim
+++ kpkg/modules/downloader.nim
@@ -3,11 +3,12 @@ import terminal, math, strutils
 
 proc onProgressChanged(total, progress, speed: BiggestInt) {.async.} =
   stdout.eraseLine
-  var p = "Downloaded "&formatSize(progress)&" of "&formatSize(total)&" at "&formatSize(speed)&"/s"
+  var p = "Downloaded "&formatSize(progress)&" of "&formatSize(
+      total)&" at "&formatSize(speed)&"/s"
 
   if round(int(progress) / int(total)*100) != "inf":
     p = p&" "&round(int(progress) / int(total)*100)"% complete"
-  
+
   stdout.write(p)
   stdout.flushFile
 
