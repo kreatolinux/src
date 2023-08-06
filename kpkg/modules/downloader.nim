061d36a3f19bc33eb4c22e69b0404b9e3d68b0e3
Kreato <kreato@kreato.dev>
2023-08-06 09:31:20 +0300

    kpkg: downloader: if progress can't be calculated dont show it

--- kpkg/modules/downloader.nim
+++ kpkg/modules/downloader.nim
@@ -3,8 +3,12 @@ import terminal, math, strutils
 
 proc onProgressChanged(total, progress, speed: BiggestInt) {.async.} =
   stdout.eraseLine
-  stdout.write("Downloaded ", formatSize(progress), " of ", formatSize(total),
-      " at ", formatSize(speed), "/s ", round(int(progress) / int(total)*100), "% complete")
+  var p = "Downloaded "&formatSize(progress)&" of "&formatSize(total)&" at "&formatSize(speed)&"/s"
+
+  if round(int(progress) / int(total)*100) != "inf":
+    p = p&" "&round(int(progress) / int(total)*100)"% complete"
+  
+  stdout.write(p)
   stdout.flushFile
 
 proc download*(url: string, file: string) {.async.} =
