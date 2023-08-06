7aeeff8181de7d125e975ac04de00e221b805984
Kreato <kreato@kreato.dev>
2023-08-06 09:38:05 +0300

    downloader: fix syntax error, also set precision to -1 so it looks nicer

--- kpkg/modules/downloader.nim
+++ kpkg/modules/downloader.nim
@@ -7,7 +7,7 @@ proc onProgressChanged(total, progress, speed: BiggestInt) {.async.} =
       total)&" at "&formatSize(speed)&"/s"
 
   if round(int(progress) / int(total)*100) != "inf":
-    p = p&" "&round(int(progress) / int(total)*100)"% complete"
+    p = p&" "&formatBiggestFloat(round(int(progress) / int(total)*100), precision = -1)&"% complete"
 
   stdout.write(p)
   stdout.flushFile
