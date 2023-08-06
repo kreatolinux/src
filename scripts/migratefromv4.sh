a39c38a6d4ee17f1143de4871fa3cc3871a8ace4
Kreato <kreato@kreato.dev>
2023-08-06 10:55:13 +0300

    scripts: improve on migratefromv4

--- scripts/migratefromv4.sh
+++ scripts/migratefromv4.sh
@@ -2,29 +2,32 @@
 # Script to migrate a package from v4 to v5
 # Usage: sh migratefromv4.sh PACKAGEPATH
 
-[ ! -d "$1" ] && exit 1
+[ ! -f "$1/run" ] && exit 1
 
 sed -i 's/install()/package()/g' "$1/run"
 
-cat "$1/deps" | while read line 
-do
-   if [ "$DEPS" = "" ]; then
-    DEPS="$line"
-   else
-    DEPS="$DEPS $line"
-   fi
-done
+if [ -f "$1/build_deps" ]; then
+    cat "$1/build_deps" | while read line 
+    do
+        if [ "$DEPS" = "" ]; then
+            BDEPS="$line"
+        else
+            BDEPS="$BDEPS $line"
+        fi
+    done
+    sed -i "1n; /^SHA256SUM/i BUILD_DEPENDS=\"$BDEPS\"" "$1/run"
+fi
 
-cat "$1/build_deps" | while read line 
-do
-   if [ "$DEPS" = "" ]; then
-    BDEPS="$line"
-   else
-    BDEPS="$BDEPS $line"
-   fi
-done
-
-sed -i "1n; /^SHA256SUM/i BUILD_DEPENDS=\"$BDEPS\"" "$1/run"
-sed -i "1n; /^BUILD_DEPENDS/i DEPENDS=\"$DEPS\"" "$1/run"
+if [ -f "$1/deps" ]; then
+    cat "$1/deps" | while read line 
+    do
+        if [ "$DEPS" = "" ]; then
+            DEPS="$line"
+        else
+            DEPS="$DEPS $line"
+        fi
+    done
+    sed -i "1n; /^BUILD_DEPENDS/i DEPENDS=\"$DEPS\"" "$1/run"
+fi
 
 echo "complete"
