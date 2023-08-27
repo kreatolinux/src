0b1f7a431e3600209c50ce9224d545d2a8ec2133
Kreato <kreato@kreato.dev>
2023-08-27 16:52:09 +0300

    aaaaaaaaa

--- kpkg/modules/removeInternal.nim
+++ kpkg/modules/removeInternal.nim
@@ -4,32 +4,29 @@ import strutils
 import sequtils
 import runparser
 
-proc removeInternal*(package: string, root = "",
-        installedDir = "/var/cache/kpkg/installed",
-        ignoreReplaces = false): string =
-
+proc removeInternal*(package: string, root = "", installedDir = root&"/var/cache/kpkg/installed", ignoreReplaces = false): string =
+    
     var actualPackage: string
 
-    if symlinkExists(root&"/var/cache/kpkg/installed/"&package):
-        actualPackage = expandSymlink(root&installedDir&"/"&package)
+    if symlinkExists(installedDir&"/"&package):
+        actualPackage = expandSymlink(installedDir&"/"&package)
     else:
         actualPackage = package
 
-    if not fileExists(root&installedDir&"/"&actualPackage&"/list_files"):
+    if not fileExists(installedDir&"/"&actualPackage&"/list_files"):
         err("package "&package&" is not installed", false)
 
-    for line in lines root&installedDir&"/"&actualPackage&"/list_files":
+    for line in lines installedDir&"/"&actualPackage&"/list_files":
         discard tryRemoveFile(root&"/"&line)
 
         if isEmptyOrWhitespace(toSeq(walkDirRec(root&"/"&line)).join("")):
             removeDir(root&"/"&line)
 
-    removeDir(root&installedDir&"/"&package)
-
+    removeDir(installedDir&"/"&package)
+    
     if not ignoreReplaces:
-        let pkgreplaces = parse_runfile(
-                root&installedDir&"/"&actualPackage).replaces
+        let pkgreplaces = parse_runfile(installedDir&"/"&actualPackage).replaces
         for i in pkgreplaces:
-            removeDir(root&installedDir&"/"&i)
+            removeDir(installedDir&"/"&i)
 
     return "kpkg: package "&package&" removed."
