1493ea17aacc0305ba7a5076a0db6cb0fda51913
Kreato <kreato@kreato.dev>
2023-08-30 20:12:49 +0300

    uhhhhh hope this fixes everything

--- kpkg/commands/installcmd.nim
+++ kpkg/commands/installcmd.nim
@@ -48,7 +48,8 @@ proc install_pkg*(repo: string, package: string, root: string) =
     if dirExists(root&"/var/cache/kpkg/installed/"&package):
 
         echo "kpkg: package already installed, reinstalling"
-
+        
+        removeDir("/tmp/kpkg")
         createDir("/tmp")
         createDir("/tmp/kpkg")
         createDir("/tmp/kpkg/reinstall")
@@ -87,12 +88,11 @@ proc install_pkg*(repo: string, package: string, root: string) =
 
     if dirExists("/tmp/kpkg/reinstall/"&package&"-old"):
         let cmd = execProcess("grep -xvFf /tmp/kpkg/reinstall/"&package&"-old/list_files /var/cache/kpkg/installed/"&package&"/list_files")
-        echo cmd
         if not isEmptyOrWhitespace(cmd):
             writeFile("/tmp/kpkg/reinstall/list_files", cmd)
             discard removeInternal("reinstall", root,
                     installedDir = "/tmp/kpkg", ignoreReplaces = true)
-            removeDir("/tmp/kpkg")
+        removeDir("/tmp/kpkg")
 
     var existsPostinstall = execCmdEx(
             ". "&repo&"/"&package&"/run"&" && command -v postinstall").exitCode
