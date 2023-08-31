eafa66bf628d9d4faf6efdd7d39eaf8015380e3c
Kreato <kreato@kreato.dev>
2023-08-31 05:51:43 +0300

    this commit is proof that i should sleep

--- kpkg/commands/installcmd.nim
+++ kpkg/commands/installcmd.nim
@@ -87,7 +87,7 @@ proc install_pkg*(repo: string, package: string, root: string) =
     
     var listFiles: seq[string]
 
-    extractAll("kpkg-tarball-kmod-30-3.tar.gz", "/tmp/kpkg/extractDir")
+    extractAll(tarball, "/tmp/kpkg/extractDir")
     setCurrentDir("/tmp/kpkg/extractDir")
     for i in walkDirRec(".", {pcFile, pcLinkToFile, pcDir, pcLinkToDir}):
       listFiles = listFiles&listFiles
