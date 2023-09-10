f3ad1f51a3dbf99ad0ee370dfd977666c7363a5e
Kreato <kreato@kreato.dev>
2023-09-10 18:39:15 +0300

    kpkg: logger: add info, warning and make them colorful

--- kpkg/modules/logger.nim
+++ kpkg/modules/logger.nim
@@ -1,9 +1,23 @@
 import os
+import terminal
 
 proc err*(error: string, removeLockFile = true) =
     ## Handles errors.
-    echo("kpkg: error: "&error)
+    styledEcho("kpkg: ", fgRed, "error: ", fgDefault, error)
     if removeLockFile:
         echo "kpkg: removing lockfile"
         removeFile("/tmp/kpkg.lock")
     quit(1)
+
+proc warn*(warn: string) =
+    ## Handles warnings.
+    styledEcho("kpkg: ", fgYellow, "warning: ", fgDefault, warn)
+
+proc success*(success: string, exitAfterwards = false) =
+    ## Handles success messages.
+    styledEcho("kpkg: ", fgGreen, "success: ", fgDefault, success)
+
+proc info*(info: string) =
+    ## Handles info messages.
+    styledEcho("kpkg: ", fgBlue, "info: ", fgDefault, info)
+    
