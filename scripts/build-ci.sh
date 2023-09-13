69e35981666c5a00f921e1b1bd4baf0b976bafd1
Kreato <kreato@kreato.dev>
2023-09-13 13:10:23 +0300

    temporary shit

--- scripts/build-ci.sh
+++ scripts/build-ci.sh
@@ -32,6 +32,7 @@ case $1 in
                 echo "int main() { return 0; }" >> test.c
                 clang test.c || exit 1
                 ./a.out || exit 1
+                cp kreastrap/overlay/etc/profile /etc/profile || exit 1
                 #rm -f /var/cache/kpkg/archives/*kpkg*
                 rm -rf /tmp/kpkg
                 nim c -d:branch=master --passC:-no-pie --threads:on -d:ssl -o=kreastrap/kreastrap kreastrap/kreastrap.nim
