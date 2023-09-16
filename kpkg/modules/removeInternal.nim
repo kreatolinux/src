4a47be97cb46dd2f7f51f6c323909b663addcb72
Kreato <kreato@kreato.dev>
2023-09-16 15:48:01 +0300

    kpkg: removeInternal: check dependencies

--- kpkg/modules/removeInternal.nim
+++ kpkg/modules/removeInternal.nim
@@ -3,44 +3,54 @@ import logger
 import strutils
 import sequtils
 import runparser
+import dephandler
 import commonTasks
+
+proc dependencyCheck(package: string, installedDir: string, root: string) =
+  ## Checks if a package is a dependency on another package.
+  let d = dephandler(toSeq(walkDirs(installedDir&"/*")), root = root)
+  for i in d:
+    if i == package:
+      err(i&" is dependent on "&package)
 
 proc removeInternal*(package: string, root = "",
         installedDir = root&"/var/cache/kpkg/installed",
         ignoreReplaces = false) =
 
-    let init = getInit(root)
+  let init = getInit(root)
 
-    if dirExists(installedDir&"/"&package&"-"&init):
-        removeInternal(package&"-"&init, root, installedDir, ignoreReplaces)
+  if dirExists(installedDir&"/"&package&"-"&init):
+    removeInternal(package&"-"&init, root, installedDir, ignoreReplaces)
 
-    var actualPackage: string
+  var actualPackage: string
 
-    if symlinkExists(installedDir&"/"&package):
-        actualPackage = expandSymlink(installedDir&"/"&package)
-    else:
-        actualPackage = package
+  if symlinkExists(installedDir&"/"&package):
+    actualPackage = expandSymlink(installedDir&"/"&package)
+  else:
+    actualPackage = package
 
-    if not dirExists(installedDir&"/"&actualPackage):
-        err("package "&package&" is not installed", false)
+  if not dirExists(installedDir&"/"&actualPackage):
+    err("package "&package&" is not installed", false)
 
-    let pkg = parse_runfile(installedDir&"/"&actualPackage)
+  let pkg = parse_runfile(installedDir&"/"&actualPackage)
 
-    if not pkg.isGroup:
-        if not fileExists(installedDir&"/"&actualPackage&"/list_files"):
-            warn "Package doesn't have a file list. Possibly broken package? Removing anyway."
-            removeDir(installedDir&"/"&package)
-            return
+  dependencyCheck(package, installedDir, root)
 
-        for line in lines installedDir&"/"&actualPackage&"/list_files":
-            discard tryRemoveFile(root&"/"&line)
+  if not pkg.isGroup:
+    if not fileExists(installedDir&"/"&actualPackage&"/list_files"):
+      warn "Package doesn't have a file list. Possibly broken package? Removing anyway."
+      removeDir(installedDir&"/"&package)
+      return
 
-            if isEmptyOrWhitespace(toSeq(walkDirRec(root&"/"&line)).join("")):
-                removeDir(root&"/"&line)
+    for line in lines installedDir&"/"&actualPackage&"/list_files":
+      discard tryRemoveFile(root&"/"&line)
 
-    if not ignoreReplaces:
-        for i in pkg.replaces:
-            if symlinkExists(installedDir&"/"&i):
-                removeFile(installedDir&"/"&i)
+      if isEmptyOrWhitespace(toSeq(walkDirRec(root&"/"&line)).join("")):
+        removeDir(root&"/"&line)
 
-    removeDir(installedDir&"/"&package)
+  if not ignoreReplaces:
+    for i in pkg.replaces:
+      if symlinkExists(installedDir&"/"&i):
+        removeFile(installedDir&"/"&i)
+
+  removeDir(installedDir&"/"&package)
