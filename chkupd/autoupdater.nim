c7869e0d8c1f0a795c575f3b92c5c602cd8178a4
Kreato <kreato@kreato.dev>
2023-09-22 01:34:52 +0300

    chkupd/autoupdater: Update for new runfile format (v6)

--- chkupd/autoupdater.nim
+++ chkupd/autoupdater.nim
@@ -14,8 +14,8 @@ proc autoUpdater*(pkg: runFile, packageDir: string, newVersion: string,
         var source: string
         var filename: string
 
-        for i in pkg.sha256sum.split(";"):
-                source = pkg.sources.split(";")[c].replace(pkg.version, newVersion)
+        for i in pkg.sha256sum.split(" "):
+                source = pkg.sources.split(" ")[c].replace(pkg.version, newVersion)
                 filename = extractFilename(source).strip().replace(pkg.version, newVersion)
 
                 # Download the source
@@ -30,7 +30,7 @@ proc autoUpdater*(pkg: runFile, packageDir: string, newVersion: string,
                 writeFile(packageDir&"/run", readFile(
                                 packageDir&"/run").replace(pkg.sha256sum.split(
                                 ";")[c], sha256hexdigest(readFile(
-                                filename))&"  "&filename))
+                                filename)))
                 c = c+1
 
                 # Replace the version
