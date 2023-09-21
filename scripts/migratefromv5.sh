35b9982971c4e6e8be36fe817eb36729e0bfcf28
Kreato <kreato@kreato.dev>
2023-09-21 07:07:36 +0300

    migratefromv5: allow migration to new runfile changes

--- scripts/migratefromv5.sh
+++ scripts/migratefromv5.sh
@@ -3,3 +3,5 @@
 # Usage: sh migratefromv5.sh PACKAGEPATH
 [ ! -f "$1/run" ] && exit 1
 awk '/^SHA256SUM=/ {gsub(/  .*/, "\"")} 1' "$1/run" > tmp_run && mv tmp_run "$1/run"
+sed '/^SHA256SUM/s/;/ /g' "$1/run" > tmp_run && mv tmp_run "$1/run"
+sed '/^SOURCES/s/;/ /g' "$1/run" > tmp_run && mv tmp_run "$1/run"
