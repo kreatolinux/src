97711781c9b7fb2d388ac2172123bc4df3708d3e
Kreato <kreato@kreato.dev>
2023-09-27 00:59:06 +0300

    feat: add build.sh

--- Makefile
+++ Makefile
@@ -1,38 +1,18 @@
-SRCDIR = $(shell pwd)
-PREFIX = ./out
-
-nimbuild = nim c -d:release --passL:-larchive -d:branch=master --threads:on -d:ssl -o=$(PREFIX)/$1 $(SRCDIR)/$1/$1.nim
-tasks = kpkg chkupd purr
+SRCDIR=$(shell pwd)
+tasks = kpkg chkupd purr jumpstart genpkglist kreaiso kreastrap
 
 all: $(tasks)
 
 deps:
-	nimble install fuzzy futhark cligen libsha fusion -y
+	sh build.sh -i
 
 $(tasks)::
-	$(call nimbuild,$@)
-
-jumpstart:
-	nim c -d:debug --threads:on -o=$(PREFIX)/jumpstart $(SRCDIR)/jumpstart/jumpstart.nim
-	nim c -d:debug -o=$(PREFIX)/jumpctl $(SRCDIR)/jumpstart/jumpctl.nim
-
-genpkglist:
-	nim c -d:release -d:branch=master -o=$(PREFIX)/genpkglist $(SRCDIR)/genpkglist/main.nim
-
-kreastrap:
-	nim c -d:release --passL:-larchive -d:branch=master --threads:on -d:ssl -o=$(SRCDIR)/kreastrap/kreastrap $(SRCDIR)/kreastrap/kreastrap.nim
-
-kreaiso:
-	nim c -d:release -d:branch=master --threads:on -d:ssl -o=$(SRCDIR)/kreaiso/kreaiso $(SRCDIR)/kreaiso/kreaiso
-
-prettify:
-	find $(SRCDIR) -type f -name '*.nim' | xargs nimpretty
+	sh build.sh -p $@
 
 install_klinstaller:
-	cp $(SRCDIR)/installer/klinstaller /bin/klinstaller
-	chmod +x /bin/klinstaller
+	sh build.sh -p install_klinstaller
 
 clean:
-	rm -rf $(PREFIX)
+	sh build.sh -c
 
-.PHONY: $(tasks) jumpstart kreastrap kreaiso genpkglist
+.PHONY: $(tasks)
---
+++ build.sh
@@ -0,0 +1,80 @@
+#!/bin/sh
+prefix="./out"
+srcdir="$PWD"
+projects=""
+buildType="release"
+branch="master"
+installDeps="0"
+
+err() {
+	echo "$@"
+	exit 1
+}
+
+buildNimMain() {
+	nim c -d:$buildType --passL:-larchive -d:branch=$branch --threads:on -d:ssl -o="$prefix/$1" "$srcdir/$1/$1.nim" || err "building $1 failed"
+}
+
+buildNimOther() {
+	nim c -d:$buildType --threads:$2 -o="$3" $5 "$srcdir/$1/$4" || err "building $1 failed"
+}
+
+echo "Kreato Linux - Source tree build script"
+
+while [ "$#" -gt 0 ]; do
+	case $1 in
+		-d|--debug)
+			buildType="debug"
+		;;
+		-p|--projects)
+			shift
+			projects="$1"
+		;;
+		-i|--installDeps)
+			installDeps="1"
+		;;
+		-c|--clean)
+			echo "Cleaning binaries"
+			rm -rf "$prefix"
+			rm -f $srcdir/kreastrap/kreastrap
+			rm -f $srcdir/kreaiso/kreaiso
+		;;
+		-b|--branch)
+			shift
+			branch="$1"
+		;;
+	esac
+	shift
+done
+
+if [ "$installDeps" = "1" ]; then
+    nimble install fuzzy futhark cligen libsha fusion -y || err "Installing depndencies failed"
+fi
+
+IFS=","
+for v in $projects
+do
+   echo "building $v"
+   case $v in
+	kpkg | chkupd | purr)
+		buildNimMain "$v"
+	;;
+	jumpstart)
+		buildNimOther "jumpstart" "on" "$prefix/jumpstart" "jumpstart.nim"
+		buildNimOther "jumpstart" "off" "$prefix/jumpctl" "jumpctl.nim"
+	;;
+	genpkglist)	
+		buildNimOther "genpkglist" "off" "$prefix/genpkglist" "main.nim"
+   	;;
+	kreastrap)
+		buildNimOther "kreastrap" "on" "$srcdir/kreastrap/kreastrap" "kreastrap.nim" "--passL:-larchive -d:branch=$branch -d:ssl"
+	;;
+	kreaiso)
+		buildNimOther "kreaiso" "on" "$srcdir/kreaiso/kreaiso" "kreaiso.nim" "--passL: -larchive -d:branch=$branch -d:ssl"
+	;;
+	install_klinstaller)
+		cp "$srcdir/installer/klinstaller" /bin/klinstaller
+		chmod +x /bin/klinstaller
+	;;
+   esac
+done
