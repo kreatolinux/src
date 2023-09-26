09d364ced303dfe7c87ee212ef21064474749206
Kreato <kreato@kreato.dev>
2023-09-27 01:30:36 +0300

    Update build-ci.sh

--- scripts/build-ci.sh
+++ scripts/build-ci.sh
@@ -59,7 +59,7 @@ EOF
     		make deps kpkg
 		./out/kpkg update
   		./out/kpkg install xz-utils -y || exit 1
-  		./out/kpkg build llvm -y
+  		./out/kpkg install llvm -y
   		cd ..
 
       		export PATH=$PATH:$HOME/.nimble/bin
