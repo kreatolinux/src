99f11aa3214d1f48a333432dbb35d1e46f3840dc
Kreato <kreato@kreato.dev>
2023-12-10 00:39:38 +0300

    kpkg: add  some exports

--- kpkg/modules/runFileExtraCommands.sh
+++ kpkg/modules/runFileExtraCommands.sh
@@ -5,6 +5,9 @@ export KPKG_CONFIGURE_PATH="./configure"
 
 make() {
   if [ "$KPKG_ARCH" != "$(uname -m)" ]; then
+    export CFLAGS="$CFLAGS -I/usr/$KPKG_TARGET/usr/include"
+    export LDFLAGS="$LDFLAGS -L/usr/$KPKG_TARGET/lib"
+    export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:/usr/$KPKG_TARGET/usr/lib/pkgconfig"
     env make ARCH="$KPKG_ARCH" CROSS_COMPILE="$KPKG_TARGET-" $@
   else
      env make $@
@@ -13,6 +16,9 @@ make() {
 
 kpkgConfigure() {
   if [ "$KPKG_ARCH" != "$(uname -m)" ]; then
+    export CFLAGS="$CFLAGS -I/usr/$KPKG_TARGET/usr/include"
+    export LDFLAGS="$LDFLAGS -L/usr/$KPKG_TARGET/lib"
+    export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:/usr/$KPKG_TARGET/usr/lib/pkgconfig"
     $KPKG_CONFIGURE_PATH --prefix=/usr --host="$KPKG_TARGET" --build="$KPKG_HOST_TARGET" $@
   else
     $KPKG_CONFIGURE_PATH --prefix=/usr $@
@@ -21,11 +27,21 @@ kpkgConfigure() {
   
 
 cmake() {
-  env cmake -DCMAKE_TOOLCHAIN_FILE=/usr/$KPKG_TARGET/$KPKG_TARGET.cmake $@
+  if [ "$KPKG_ARCH" != "$(uname -m)" ]; then
+    export CFLAGS="$CFLAGS -I/usr/$KPKG_TARGET/usr/include"
+    export LDFLAGS="$LDFLAGS -L/usr/$KPKG_TARGET/lib"
+    export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:/usr/$KPKG_TARGET/usr/lib/pkgconfig"
+    env cmake -DCMAKE_TOOLCHAIN_FILE=/usr/$KPKG_TARGET/$KPKG_TARGET.cmake $@
+  else
+    env cmake $@
+  fi
 }
 
 meson() {
   if [ "$KPKG_ARCH" != "$(uname -m)" ]; then
+    export CFLAGS="$CFLAGS -I/usr/$KPKG_TARGET/usr/include"
+    export LDFLAGS="$LDFLAGS -L/usr/$KPKG_TARGET/lib"
+    export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:/usr/$KPKG_TARGET/usr/lib/pkgconfig"
     env meson --cross-file "/usr/$KPKG_TARGET/$KPKG_TARGET-meson.txt" $@
   else
     env meson $@
