46219a9e20b3d8b32cbf00dd15090526c13c9994
Kreato <kreato@kreato.dev>
2023-12-14 02:16:36 +0300

    Update build-ci.sh

--- scripts/build-ci.sh
+++ scripts/build-ci.sh
@@ -20,8 +20,8 @@ case $1 in
     		kpkg build llvm -y # Required by futhark
 
   		kpkg install wget -y
-      		wget https://mirror.kreato.dev/arch/aarch64/kpkg-tarball-glibc-2.38-3.tar.gz || exit 1
-		wget https://mirror.kreato.dev/arch/aarch64/kpkg-tarball-glibc-2.38-3.tar.gz.sum || exit 1
+      		wget https://mirror.kreato.dev/aarch64/kpkg-tarball-glibc-2.38-3.tar.gz || exit 1
+		wget https://mirror.kreato.dev/aarch64/kpkg-tarball-glibc-2.38-3.tar.gz.sum || exit 1
 		mkdir /var/cache/kpkg/archives/aarch64 || exit 1
   		mv kpkg-tarball-glibc-* /var/cache/kpkg/archives/aarch64 || exit 1
   
