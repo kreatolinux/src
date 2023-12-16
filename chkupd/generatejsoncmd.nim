02d9b967297f6e0ead7423c267b1e3d53f980c02
Kreato <kreato@kreato.dev>
2023-12-16 05:30:37 +0300

    chkupd: generatejsoncmd: more efficient json generation

--- chkupd/generatejsoncmd.nim
+++ chkupd/generatejsoncmd.nim
@@ -1,31 +1,51 @@
 include json
 import os
+import ../kpkg/modules/runparser
 
-proc generateJson*(backend = "repology", repo: string, limit = 256, autoUpdate = true, output = "out.json") =
+
+proc generateJson*(backend = "repology", repo: string, limit = 256, output = "out.json") =
     ## Generates a build jsonfile
     type
         update = object
-            package: string
+            packages: string
             repository: string
-            command: string
     
-    var u: seq[update]
+    var updateList: seq[update]
 
     let repoFullPath = absolutePath(repo)
     var count: int
 
     for i in walkFiles(repoFullPath&"/*/run"):
         
+        let pkg = lastPathPart(i.parentDir)
+
         if count >= limit and limit > 0:
             break
+        
+        var deps = parseRunfile(i.parentDir).deps.join(" ") # Not full dependency list ofc, but good enough for now
+        
+        var willContinue: bool
 
-        let pkg = lastPathPart(i.parentDir)
-        u = u&update(package: pkg, repository: repoFullPath, command: "chkupd "&backend&"Check --package="&pkg&" --repo="&repoFullPath&" --autoUpdate="&($autoUpdate))
+        for i in updateList:
+                if pkg in i.packages.split(" "):
+                    willContinue = true
+        
+        if willContinue:
+            continue
+
+        if not isEmptyOrWhitespace(deps):
+            deps = deps&" "&pkg
+        else:
+            deps = pkg
+        
+        updateList = updateList&update(packages: deps, repository: repoFullPath)
         count = count + 1
+    
+    echo count
 
     let res = %*
         {
-            "include": %u
+            "include": %updateList
         }
 
     writeFile(output, $(res))
