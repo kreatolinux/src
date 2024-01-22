ccf8a7e03f083980bf915a1e8ec5e9de29552b93
Kreato <kreato@kreato.dev>
2024-01-22 09:07:02 +0300

    chkupd: generateJson: add splitIfLimit and ignorePackages arguments

--- chkupd/generatejsoncmd.nim
+++ chkupd/generatejsoncmd.nim
@@ -3,14 +3,10 @@ import os
 import ../kpkg/modules/runparser
 
 
-proc generateJson*(backend = "repology", repo: string, limit = 256, output = "out.json") =
+proc generateJson*(repo: string, limit = 256, splitIfLimit = true, output = "out.json", ignorePackages = @[""]) =
     ## Generates a build jsonfile
-    type
-        update = object
-            packages: string
-            repository: string
     
-    var updateList: seq[update]
+    var updateList: seq[string]
 
     let repoFullPath = absolutePath(repo)
     var count: int
@@ -18,27 +14,35 @@ proc generateJson*(backend = "repology", repo: string, limit = 256, output = "ou
     for i in walkFiles(repoFullPath&"/*/run"):
         
         let pkg = lastPathPart(i.parentDir)
-
-        if count >= limit and limit > 0:
-            break
-        
-        var deps = parseRunfile(i.parentDir).deps.join(" ") # Not full dependency list ofc, but good enough for now
         
         var willContinue: bool
 
+        for i in ignorePackages:
+            if pkg in i.split(" "):
+                willContinue = true
+
+        if count >= limit and limit > 0:
+            if splitIfLimit:
+                let outputSplit = output.split(".")
+                generateJson(repo, limit, splitIfLimit, output = outputSplit[0]&"-2."&outputSplit[1], ignorePackages = ignorePackages&updateList)
+            
+            break
+        
         for i in updateList:
-                if pkg in i.packages.split(" "):
+                if pkg in i.split(" "):
                     willContinue = true
         
         if willContinue:
             continue
 
+        var deps = parseRunfile(i.parentDir).deps.join(" ") # Not full dependency list ofc, but good enough for now 
+        
         if not isEmptyOrWhitespace(deps):
             deps = deps&" "&pkg
         else:
             deps = pkg
         
-        updateList = updateList&update(packages: deps, repository: repoFullPath)
+        updateList = updateList&deps
         count = count + 1
     
     echo count
