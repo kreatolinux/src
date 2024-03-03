876746e753b7feddab81486b498073b46a74a7eb
Kreato <kreato@kreato.dev>
2024-03-03 16:44:47 +0300

    scripts/build-ci: remove a lot of temp stuff

--- scripts/build-ci.sh
+++ scripts/build-ci.sh
@@ -18,57 +18,26 @@ case $1 in
                         git config --global --add safe.directory $GITHUB_WORKSPACE
                 fi
                 
-		mkdir /out
+		            mkdir /out
                 ln -s /out /work/out
                 cd /work || exit 1
 
-  		kpkg # Initializes configs
-		sed -i s/stable/master/g /etc/kpkg/kpkg.conf # Switch to master repos
-		kpkg update
-  		rm -f /var/cache/kpkg/archives/arch/amd64/*wget*
-      		kpkg build bzip2 -y || exit 1
+  	            kpkg # Initializes configs
+		            sed -i s/stable/master/g /etc/kpkg/kpkg.conf # Switch to master repos
+		            kpkg update
+                kpkg build bzip2 -y || exit 1
     
-    # tmp
-		kpkg build grep ncurses attr xz-utils shadow gtar bzip2 busybox -f -yu
-    kpkg install grep ncurses attr xz-utils shadow gtar bzip2 busybox -y
-    kpkg build binutils -f -y
-    # tmp end
+                kpkg build python -y || exit 1
 
-    kpkg build python -y || exit 1
-    		python -m ensurepip -U
-		ln -s $(which pip3) /usr/bin/pip
-		pip --version || exit 1
-    		kpkg build python-pip -y
-		pip --version || exit 1
-
-		kpkg build ninja -y
-    		kpkg build llvm -y # Required by futhark
-    		kpkg build sqlite -y # Required by kpkg audit
-            kpkg build bubblewrap -y # Required by kpkg isolation
+		            kpkg build ninja -y
+                kpkg build llvm -y # Required by futhark
 
         
-        # Hack to install kreato-fs-essentials until we get a working build
-        git clone https://github.com/kreatolinux/src src-old
-        cd src-old
-        git checkout a8c8e28b73daac823fa029845c7c66891b80d093
-		export PATH=$PATH:$HOME/.nimble/bin # Add nimble path so opir can run
-        ./build.sh -i
-        ./build.sh -d -p kpkg
-        ./out/kpkg build p11-kit ca-certificates -y || exit 1
-        update-ca-trust || exit 1
-        ./out/kpkg build kreato-fs-essentials -y
-        ./out/kpkg build pcre bash binutils bison flex gcc gettext glibc gmake gmp grep gtar libffi libtasn m4 mpc mpfr readline texinfo -y || exit 1 # https://github.com/kreatolinux/kpkg-repo/commit/a5891e1b1b1f0645fb48058bc1d015ea54bcbb93
-        cd ..
-        rm -rf src-old
-        
-        rm -vf /etc/kpkg/kpkg.conf
-        rm -rf /tmp/kpkg
-
-
-  		./build.sh -i
-		nim c -d:branch=master --passL:-larchive --passC:-no-pie --threads:on -d:ssl -o=kreastrap/kreastrap kreastrap/kreastrap.nim
+                export PATH=$PATH:$HOME/.nimble/bin # Add nimble path so opir can run
+  	            ./build.sh -i
+		            nim c -d:branch=master --passL:-larchive --passC:-no-pie --threads:on -d:ssl -o=kreastrap/kreastrap kreastrap/kreastrap.nim
                 
-		cat /etc/group | grep tty || addgroup tty
+		            cat /etc/group | grep tty || addgroup tty
         ;;
 
         "build")
