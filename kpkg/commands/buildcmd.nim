dfa90284e7b7e78ddced5ba93a5e1fabac8177da
Kreato <kreato@kreato.dev>
2024-03-12 08:48:53 +0300

    a

--- kpkg/commands/buildcmd.nim
+++ kpkg/commands/buildcmd.nim
@@ -577,7 +577,7 @@ proc build*(no = false, yes = false, root = "/",
             discard mountOverlay(error = "mounting overlay")
             # We set isBuild to false here as we don't want build dependencies of other packages on the sandbox.
             depsToClean = deduplicate(parseRunfile(findPkgRepo(i)&"/"&i).bdeps&dephandler(@[i], isBuild = false, root = fullRootPath, forceInstallAll = true, isInstallDir = isInstallDir, ignoreInit = ignoreInit))
-            if target != "default":
+            if target != "default" and target != kpkgTarget("/"):
                 for d in depsToClean:
                     installPkg(findPkgRepo(d), d, kpkgOverlayPath&"/upperDir", isUpgrade = false, kTarget = target, manualInstallList = @[d])
             else:
