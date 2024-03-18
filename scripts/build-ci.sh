68f473a1a4e41d168d969355c8d5d5e7cfb237cb
Kreato <kreato@kreato.dev>
2024-03-18 11:45:50 +0300

    a

--- scripts/build-ci.sh
+++ scripts/build-ci.sh
@@ -8,6 +8,7 @@ e() {
     dmesg -T| grep -E -i -B100 'killed process'
     sqlite3 /var/cache/kpkg/env/var/cache/kpkg/kpkg.sqlite .dump
     sqlite3 /opt/kpkg/overlay/upperDir/var/cache/kpkg/kpkg.sqlite .dump
+    ls -l /opt/kpkg/overlay/upperDir/var/cache/kpkg/kpkg.sqlite
     exit 1
 }
 
