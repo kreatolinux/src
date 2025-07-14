a216da64894901bf51bb9e068ddff877ee99c9a8
Kreato <kreato@kreato.dev>
2025-07-15 00:52:24 +0300

    kpkg/modules/builder/sources: more sourceDownloader fixes

--- kpkg/modules/builder/sources.nim
+++ kpkg/modules/builder/sources.nim
@@ -158,7 +158,7 @@ proc sourceDownloader*(runf: runFile, pkgName: string, sourceDir: string, runFil
         
         # Skip extraction for Git repositories as they're already in the correct format
         # And also skip localfiles
-        if runf.extract and not (source.startsWith("git::") or fileExists(localPath)):
+        if runf.extract and not (source.startsWith("git::") or (fileExists(localPath) or dirExists(localPath))):
             extractSources(sourcePath, sourceDir)
 
         i += 1
